3776522bae4da1ae254fb6cf22abf0b2aea79865
bakageddy <gdineshda@gmail.com>
2026-03-18 01:43:36 +0530

    add: new tables for threaddump'

--- schema.sql
+++ schema.sql
@@ -1,12 +1,15 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS stuckthread_meta(
-	stack_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	stack_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	thread_id INTEGER NOT NULL,
 	start INTEGER NOT NULL,
 	active_duration_ms INTEGER NOT NULL,
 	active_monitor_count_start INTEGER NULL,
 	active_monitor_count_end INTEGER NULL,
 	thread_name TEXT NULL,
-	api_request TEXT NULL
+	api_request TEXT NULL,
+	FOREIGN KEY (stack_id) REFERENCES stuckthread_stack(stack_id) DEFERRABLE INITIALLY DEFERRED
 );
 
 CREATE TABLE IF NOT EXISTS stuckthread_stack(
@@ -16,4 +19,58 @@ CREATE TABLE IF NOT EXISTS stuckthread_stack(
 	method TEXT NOT NULL,
 	frame_source TEXT NOT NULL,
 	PRIMARY KEY (stack_id, frame_idx)
+);
+
+-- TODO: Better naming!
+CREATE TABLE IF NOT EXISTS threaddump_group(
+	group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+);
+
+CREATE TABLE IF NOT EXISTS threaddump(
+	group_id INTEGER NOT NULL,
+	snapshot_id INTEGER NOT NULL,
+	triggered_unix_ms INTEGER NOT NULL,
+	FOREIGN KEY (group_id) REFERENCES threaddump_group(group_id) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE IF NOT EXISTS threaddump_threads(
+	thread_stack_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	thread_id INTEGER NOT NULL,
+	thread_name TEXT NULL,
+	state TEXT NOT NULL,
+
+	-- TIMED_WAITING/WAITING ON
+	wait_object_id INTEGER NULL,
+
+	-- BLOCKED/WAITING TO LOCK
+	owner_id INTEGER NULL,
+	owner_name TEXT NULL,
+	lock_object_id INTEGER NULL,
+
+	UNIQUE (thread_id, thread_stack_id),
+
+	FOREIGN KEY (group_id) REFERENCES threaddump(group_id) DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY (thread_stack_id) REFERENCES threaddump_stack(thread_stack_id) DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY (wait_object_id) REFERENCES threaddump_objects(object_id) DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY (lock_object_id) REFERENCES threaddump_objects(object_id) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE IF NOT EXISTS threaddump_objects(
+	object_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	class TEXT NOT NULL,
+	identity INTEGER UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS threaddump_stack(
+	thread_stack_id INTEGER NOT NULL,
+	frame_idx INTEGER NOT NULL,
+	-- ELEMENT
+	method TEXT NOT NULL,
+	frame_source TEXT NOT NULL,
+	line_number INTEGER NULL,
+
+	-- LOCK
+	object_id NULL,
+	PRIMARY KEY (thread_stack_id, frame_idx),
+	FOREIGN KEY (object_id) REFERENCES threaddump_objects(object_id) DEFERRABLE INITIALLY DEFERRED
 );
