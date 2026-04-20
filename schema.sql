c77e692a4ff287d4e20678bc27825decb0d81889
bakageddy <gdineshda@gmail.com>
2026-04-20 14:29:42 +0530

    refactor: schema.sql

--- schema.sql
+++ schema.sql
@@ -1,42 +1,61 @@
 PRAGMA foreign_keys = ON;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
+PRAGMA cache_size = -64000;
+PRAGMA temp_store = MEMORY;
 
-CREATE TABLE IF NOT EXISTS stuckthread_meta(
-	stack_id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS object(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	class TEXT NOT NULL,
+	identity INTEGER UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stacktrace(
+	id INTEGER PRIMARY KEY AUTOINCREMENT
+);
+
+CREATE TABLE IF NOT EXISTS stacktrace_elements(
+	id INTEGER NOT NULL,
+	frame_idx INTEGER NOT NULL,
+
+	-- ELEMENT
+	method TEXT NULL,
+	frame_source TEXT NULL,
+	line_number INTEGER NULL,
+
+	-- LOCK
+	object_id INTEGER NULL,
+	PRIMARY KEY (id, frame_idx),
+	FOREIGN KEY (id) REFERENCES stacktrace(id) DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY (object_id) REFERENCES object(id) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE IF NOT EXISTS stuckthread(
 	thread_id INTEGER NOT NULL,
 	start INTEGER NOT NULL,
+	name TEXT NULL,
+	request TEXT NULL,
 	active_duration_ms INTEGER NOT NULL,
-	active_monitor_count_start INTEGER NULL,
-	active_monitor_count_end INTEGER NULL,
-	thread_name TEXT NULL,
-	api_request TEXT NULL
-);
-
-CREATE TABLE IF NOT EXISTS stuckthread_stack(
+	active_monitor_start INTEGER NULL,
+	active_monitor_end INTEGER NULL,
 	stack_id INTEGER NOT NULL,
-	frame_idx INTEGER NOT NULL,
-	line_number INTEGER NULL,
-	method TEXT NOT NULL,
-	frame_source TEXT NOT NULL,
-	PRIMARY KEY (stack_id, frame_idx),
-	FOREIGN KEY (stack_id) REFERENCES stuckthread_meta(stack_id) DEFERRABLE INITIALLY DEFERRED
+	FOREIGN KEY (stack_id) REFERENCES stacktrace(id) DEFERRABLE INITIALLY DEFERRED
 );
 
--- TODO: Better naming!
-CREATE TABLE IF NOT EXISTS threaddump_group(
-	group_id INTEGER PRIMARY KEY AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS threaddump_instance(
+	instance INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
 CREATE TABLE IF NOT EXISTS threaddump(
-	group_id INTEGER NOT NULL,
-	snapshot_id INTEGER NOT NULL,
+	instance INTEGER NOT NULL,
+	snapshot INTEGER NOT NULL,
 	triggered_unix_ms INTEGER NOT NULL,
-	FOREIGN KEY (group_id) REFERENCES threaddump_group(group_id) DEFERRABLE INITIALLY DEFERRED
+	FOREIGN KEY (instance) REFERENCES threaddump_instance(instance) DEFERRABLE INITIALLY DEFERRED
 );
 
-CREATE TABLE IF NOT EXISTS threaddump_threads(
-	thread_stack_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	thread_id INTEGER NOT NULL,
-	thread_name TEXT NULL,
+CREATE TABLE IF NOT EXISTS thread(
+	id INTEGER NOT NULL,
+	name TEXT NULL,
 	state TEXT NOT NULL,
 
 	-- TIMED_WAITING/WAITING ON
@@ -47,29 +66,14 @@ CREATE TABLE IF NOT EXISTS threaddump_threads(
 	owner_name TEXT NULL,
 	lock_object_id INTEGER NULL,
 
-	UNIQUE (thread_id, thread_stack_id),
+	-- THREAD DUMP REFERENCE KEYS
+	instance INTEGER NOT NULL,
+	snapshot INTEGER NOT NULL,
+	stack_id INTEGER NULL,
 
-	FOREIGN KEY (thread_stack_id) REFERENCES threaddump_stack(thread_stack_id) DEFERRABLE INITIALLY DEFERRED,
-	FOREIGN KEY (wait_object_id) REFERENCES threaddump_objects(object_id) DEFERRABLE INITIALLY DEFERRED,
-	FOREIGN KEY (lock_object_id) REFERENCES threaddump_objects(object_id) DEFERRABLE INITIALLY DEFERRED
-);
+	UNIQUE (id, stack_id),
 
-CREATE TABLE IF NOT EXISTS threaddump_objects(
-	object_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	class TEXT NOT NULL,
-	identity INTEGER UNIQUE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS threaddump_stack(
-	thread_stack_id INTEGER NOT NULL,
-	frame_idx INTEGER NOT NULL,
-	-- ELEMENT
-	method TEXT NOT NULL,
-	frame_source TEXT NOT NULL,
-	line_number INTEGER NULL,
-
-	-- LOCK
-	object_id INTEGER NULL,
-	PRIMARY KEY (thread_stack_id, frame_idx),
-	FOREIGN KEY (object_id) REFERENCES threaddump_objects(object_id) DEFERRABLE INITIALLY DEFERRED
+	FOREIGN KEY (stack_id) REFERENCES stacktrace(id) DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY (wait_object_id) REFERENCES object(id) DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY (lock_object_id) REFERENCES object(id) DEFERRABLE INITIALLY DEFERRED
 );
